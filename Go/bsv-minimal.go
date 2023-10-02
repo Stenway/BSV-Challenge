a1e2d49300f0183f7690da8452312da1f37f1644
Stenway <79376889+Stenway@users.noreply.github.com>
2023-10-02 19:10:56 +0200

    Add minimal Go variant

---
+++ Go/bsv-minimal.go
@@ -0,0 +1,32 @@
+﻿package main
+
+import (
+	"bytes"
+	"errors"
+	"unicode/utf8"
+)
+
+func DecodeBsv_Minimal(bsvBytes []byte) ([][]NullableString, error) {
+	result := [][]NullableString{}
+	bytesOfLines := bytes.Split(bsvBytes, []byte{0xFF})
+	for _, lineBytes := range bytesOfLines {
+		bytesOfValues := bytes.Split(lineBytes, []byte{0xFE})
+		currentLine := []NullableString{}
+		for _, valueBytes := range bytesOfValues {
+			if len(valueBytes) == 0 {
+				return nil, errors.New("Invalid BSV value byte sequence")
+			} else if len(valueBytes) == 1 && valueBytes[0] == 0xFD {
+				currentLine = append(currentLine, Null())
+			} else if len(valueBytes) == 1 && valueBytes[0] == 0xFC {
+				currentLine = append(currentLine, Str(""))
+			} else if len(valueBytes) >= 1 {
+				if !utf8.ValidString(string(valueBytes)) {
+					return nil, errors.New("Invalid string value")
+				}
+				currentLine = append(currentLine, Str(string(valueBytes)))
+			}
+		}
+		result = append(result, currentLine)
+	}
+	return result, nil
+}
